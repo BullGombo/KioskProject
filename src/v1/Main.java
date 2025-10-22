976580075257d0401c321c95692a33dcce6cf2a9
BullGombo <ormaju15123647@gmail.com>
2025-10-22 17:30:58 +0900

    🐛 fix(Main): LV 2 . 주석 아주 약간 정리
    동일

--- src/v1/Main.java
+++ src/v1/Main.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 // 프로그램 시작점으로써의 역할
 public class Main {
     // 실행 흐름 :
-    // 메뉴 텍스트 출력 -> 입력 대기 -> 번호 입력 -> 입력 번호에 맞는 기능 실행 -> 메뉴 텍스트 출력...(반복)
+// 메뉴 텍스트 출력(MenuItem접근) -> 입력 대기 -> 번호 입력 -> 입력 번호에 맞는 기능 실행
+// -> 메뉴 텍스트 출력...(반복)
 
     static Scanner sc = new Scanner(System.in);
 
