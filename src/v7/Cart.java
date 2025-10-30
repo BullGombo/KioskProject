a37ef54ec52943b17f643a4f5be6caf9d55d63b2
BullGombo <ormaju15123647@gmail.com>
2025-10-30 10:05:39 +0900

    ✨ feat(전체): 도전 LV . 2 전체 완료
    기존 컬렉션 순회 함수 스트림&람다 처리 완

--- src/v7/Cart.java
+++ src/v7/Cart.java
@@ -29,35 +29,45 @@ public class Cart<T extends CartItem> {
 
     // v7 - 장바구니에 음식 추가 (제네릭 사용 + 스트림&람다 처리)
     public void addCartItem(String itemName, int price, int quantity) {
-        // CartItem existing = cart.get(itemName);
-        T existing = cart.get(itemName); // v7 - 제네릭 처리
-        // 기존 수량과 가격에 추가하는 로직
-        if (existing != null) { // 이미 존재하던 품목 처리
-            existing.setQuantity(existing.getQuantity() + quantity);
-            existing.setcPrice(existing.getcPrice() + price * quantity);
-        } else { // 존재하지 않던 품목을 새롭게 추가하는 로직
-            // cart.put(itemName, new CartItem(quantity, price * quantity));
-            cart.put(itemName, (T) new CartItem(quantity, price * quantity));
-        }
+//        // CartItem existing = cart.get(itemName);
+//        T existing = cart.get(itemName); // v7 - 제네릭 처리
+//        // 기존 수량과 가격에 추가하는 로직
+//        if (existing != null) { // 이미 존재하던 품목 처리
+//            existing.setQuantity(existing.getQuantity() + quantity);
+//            existing.setcPrice(existing.getcPrice() + price * quantity);
+//        } else { // 존재하지 않던 품목을 새롭게 추가하는 로직
+//            // cart.put(itemName, new CartItem(quantity, price * quantity));
+//            cart.put(itemName, (T) new CartItem(quantity, price * quantity));
+//        }
+
+        // (Stream + Lambda 버전)
+        // entrySet() : (key)(value) 한 쌍을 Map.Entry 객체로 묶어서 Set 형태로 반환
+        cart.entrySet().stream()
+                // e = 스트림의 각 원소 쌍,
+                // 여기서는 cart.Map.Entry<String, CartItem>의 entrySet
+
+                // 장바구니의 각 key값이 itemName과 같은 이름의 음식이 있는지 비교
+                .filter(e -> e.getKey().equals(itemName))
+                // 유효성 검사 (존재하는지, 없는지) - 첫 번째 요소를 Optional로 반환
+                .findFirst()
+                // 조건문 - Optional의 값이 존재하는지 여부에 따라 다른 동작 수행
+                .ifPresentOrElse(
+                        e -> {
+                            // 존재할 때
+                            CartItem item = (CartItem) e.getValue();
+                            item.setQuantity(item.getQuantity() + quantity);
+                            item.setcPrice(item.getcPrice() + price * quantity);
+                        }, // 존재하지 않을 때
+                        () -> cart.put(itemName,
+                                (T) new CartItem(quantity, price * quantity))
+                );
+
         System.out.println("=> [" + itemName + "] " + quantity +
                 "개가 장바구니에 추가되었습니다.");
 
-        // 굳이! 스트림&람다를 쓴다면 아래와 같은 형식이 된다...
-//        cart.entrySet().stream()
-//                .filter(e -> e.getKey().equals(itemName))
-//                .findFirst()
-//                .ifPresentOrElse(
-//                        e -> {
-//                            CartItem item = (CartItem) e.getValue();
-//                            item.setQuantity(item.getQuantity() + quantity);
-//                            item.setcPrice(item.getcPrice() + price * quantity);
-//                        },
-//                        () -> cart.put(itemName, (T) new CartItem(quantity, price * quantity))
-//                );
-
     }
 
-    // v7 - 장바구니 출력 (스트림 + 람다)
+    // v7 - 장바구니 출력 (Stream + Lambda)
     public void showCart() {
         // 장바구니가 비어있는 경우
         if (cart.isEmpty()) {
@@ -100,7 +110,7 @@ public class Cart<T extends CartItem> {
         }
     } // showCart() 끝
 
-    // v7 - 장바구니 항목 삭제
+    // v7 - 장바구니 항목 삭제 (Stream + Lambda)
     public void deleteCartItem() {
         if (cart.isEmpty()) {
             System.out.println("삭제할 항목이 없습니다.");
@@ -151,19 +161,44 @@ public class Cart<T extends CartItem> {
             // .trim().toLowerCase(); = 대문자 입력시 소문자로 변환
             String confirm = sc.nextLine().trim().toLowerCase();
 
-            // 실제 삭제 로직
+//            // 실제 삭제 로직
+//            if (confirm.equals("y")) {
+//                // 현재 수량보다 입력된 수량이 크거나 같을 경우, 해당 키 자체를 삭제
+//                if (item.getQuantity() <= delQty) {
+//                    cart.remove(targetItem);
+//                } else { // 기존 수량과 가격에서 입력값 만큼 빼기
+//                    item.setQuantity(item.getQuantity() - delQty);
+//                    item.setcPrice(item.getcPrice() - delQty * (item.getcPrice() / item.getQuantity()));
+//                }
+//                System.out.println("삭제 완료");
+//            } else {
+//                System.out.println("삭제를 취소했습니다.");
+//            }
+            // (Stream + Lambda 버전)
             if (confirm.equals("y")) {
-                // 현재 수량보다 입력된 수량이 크거나 같을 경우, 해당 키 자체를 삭제
-                if (item.getQuantity() <= delQty) {
-                    cart.remove(targetItem);
-                } else { // 기존 수량과 가격에서 입력값 만큼 빼기
-                    item.setQuantity(item.getQuantity() - delQty);
-                    item.setcPrice(item.getcPrice() - delQty * (item.getcPrice() / item.getQuantity()));
-                }
+                cart.entrySet().stream()
+                        // targetItem과의 비교연산 후 동일한 이름의 key를 반환
+                        .filter(e -> e.getKey().equals(targetItem))
+                        // 첫번째 항목을 Optional로 반환
+                        .findFirst()
+                        // 존재 여부에 따른 조건 분기
+                        .ifPresent(e -> {
+                            // cart 형식의 Map value, 즉 수량과 가격
+                            T itemVal = e.getValue();
+                            // 현재 수량이 삭제 수량과 같거나 작을 경우 - 해당 map의 key 전체 삭제
+                            if (itemVal.getQuantity() <= delQty) {
+                                cart.remove(e.getKey());
+                            } else {
+                                itemVal.setQuantity(itemVal.getQuantity() - delQty);
+                                itemVal.setcPrice(itemVal.getcPrice() -
+                                        delQty * (itemVal.getcPrice() / itemVal.getQuantity()));
+                            }
+                        });
                 System.out.println("삭제 완료");
             } else {
                 System.out.println("삭제를 취소했습니다.");
             }
+
 
         } catch (NumberFormatException e) { // 수량 입력에 숫자가 입력되지 않았을 경우
             System.out.println("숫자를 올바르게 입력해주세요.");
